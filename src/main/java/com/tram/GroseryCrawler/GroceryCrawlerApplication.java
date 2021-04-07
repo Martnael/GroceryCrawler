72ad585df26b0d8e7fc1018e59460b0af074feda
Mart Nael <martnael84@gmail.com>
2021-04-07 19:46:16 +0300

    Initial commit

---
+++ src/main/java/com/tram/GroseryCrawler/GroceryCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.tram.GroseryCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tram/GroseryCrawler/GroseryCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tram.GroseryCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroseryCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
